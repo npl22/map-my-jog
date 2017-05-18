1f4585cb67802d881cb0fca5139cba2ee4dde2ef
npl22 <nli22@jhu.edu>
2017-05-18 10:01:01 -0700

    Add route_util.jsx for Fronted Auth routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,2 +1,11 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <Switch>
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
+    </Switch>
+  </div>
+);
