f6c9ac893cc4a7a9921f018ced025f28cbdbcc29
npl22 <nli22@jhu.edu>
2017-05-18 09:25:20 -0700

    Fix Typo, forgot to add root in ReactDOM.render()

--- frontend/joglog.jsx
+++ frontend/joglog.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<h1>Hello, World!</h1>);
+  ReactDOM.render(<h1>Hello, World</h1>, root);
 });
