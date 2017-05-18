e5a22bf264b7a8a58049eba43310929c1fc24c3e
npl22 <nli22@jhu.edu>
2017-05-17 17:48:44 -0700

    Generate user model for backend auth

---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+  validates :name, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :session_token, :name, uniqueness: true
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, pw)
+    user = User.find_by(username: username)
+    return user if user && user.is_password?(pw)
+    nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+end
