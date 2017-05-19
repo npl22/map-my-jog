d165cda7fd79051101a3af4227c0b515aa9d91ae
npl22 <nli22@jhu.edu>
2017-05-19 09:59:23 -0700

    Add first name and last name to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def create
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:)
+  end
 end
--- db/migrate/20170518003957_create_users.rb
+++ db/migrate/20170518003957_create_users.rb
@@ -3,12 +3,17 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :username, null: false
       t.string :email
+      t.string :first_name
+      t.string :last_name
       t.string :password_digest, null: false
       t.string :session_token, null: false
       t.timestamps
     end
 
     add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :first_name
+    add_index :users, :last_name
     add_index :users, :session_token, unique: true
   end
 end
