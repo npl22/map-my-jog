2745144923c167c822faf6e8139bd7908fc087b0
npl22 <nli22@jhu.edu>
2017-05-19 11:59:01 -0700

    Create splash page, nav bar, and footer files in a shared folder

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 
+// import NavBar from './nav_bar'
+import SplashPage from './splash_page';
 import { AuthRoute } from '../util/route_util';
 import SessionFormContainer from './session_form/session_form_container';
 
 const App = () => (
   <div>
+    <Route exact path="/" component={SplashPageContainer}></Route>
     <Switch>
-      <AuthRoute path="/login" component={SessionFormContainer} />
-      <AuthRoute path="/signup" component={SessionFormContainer} />
+
     </Switch>
   </div>
 );
 
 export default App;
+
+
+// <div>
+//   <header>
+//     <NavBar />
+//     <Route exact path="/" component={SplashPageContainer}></Route>
+//   </header>
+//   <Switch>
+//     <AuthRoute path="/login" component={SessionFormContainer} />
+//     <AuthRoute path="/signup" component={SessionFormContainer} />
+//   </Switch>
+// </div>
