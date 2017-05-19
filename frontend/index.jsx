ce982ee7dbd34aee1f8f18e1b848b943382db479
npl22 <nli22@jhu.edu>
2017-05-19 11:03:05 -0700

    Configure store and add the store to the entry file

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+import Root from './components/root';
+
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<h1>Hello, World</h1>, root);
+  const store = configureStore();
+  ReactDOM.render(<Root store={store}/>, root);
 });
