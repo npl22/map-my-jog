126a1368febbb4a57246c701715b98fa4f661d14
npl22 <nli22@jhu.edu>
2017-05-21 17:42:39 -0700

    Rename project from JogLog to MapMyJog

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_JogLog_session'
+Rails.application.config.session_store :cookie_store, key: '_MapMyJog_session'
