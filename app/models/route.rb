c6826f84b2a548a9a7f5345821bd49ac12cd0180
npl22 <nli22@jhu.edu>
2017-05-22 17:54:02 -0700

    Add validations and associations for Route model

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: routes
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  waypoints  :json             not null, is an Array
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Route < ApplicationRecord
+  validates :title, presence: true
+  validates :waypoints, length: { minimum: 2, message: 'Route is too short' }
+  validates :title, uniqueness:
+    { scope: :user, message: 'You already have a route with than name' }
+
+  belongs_to :user
 end
--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: routes
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  waypoints  :json             not null, is an Array
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class RouteTest < ActiveSupport::TestCase
