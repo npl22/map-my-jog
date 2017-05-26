fd0f058e5971f40674564c8f06bb6dc53e1e8378
npl22 <nli22@jhu.edu>
2017-05-26 10:28:23 -0700

    make phone image render conditionally, only on splash page

--- frontend/components/shared/footer.jsx
+++ frontend/components/shared/footer.jsx
@@ -1,59 +1,71 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const Footer = () => (
-  <div>
-    <footer>
+const Footer = ({ location }) => {
+  let phoneImage;
+  if (location.pathname === '/') {
+    phoneImage = (
       <div className="phone">
         <img className="phone-img" src="https://res.cloudinary.com/nathanpli/image/upload/v1495765311/phone_shfazn.png" />
       </div>
+    );
+  } else {
+      phoneImage = "";
+  }
 
-      <div className="links-container">
-        <ul className="links-col">
-          <li className="link-header">Social</li>
-          <li><a className="link" href="#">Like us on Facebook</a></li>
-          <li><a className="link" href="#">Follow us on Twitter</a></li>
-          <li><a className="link" href="#">MapMyRun Blog</a></li>
-        </ul>
-        <ul className="links-col">
-          <li className="link-header">Help</li>
-          <li><a className="link" href="#">Log In / Register</a></li>
-          <li><a className="link" href="#">Support</a></li>
-          <li><a className="link" href="#">Developer / API</a></li>
-        </ul>
-        <ul className="links-col">
-          <li className="link-header">About Us</li>
-          <li><a className="link" href="#">Advertise</a></li>
-          <li><a className="link" href="#">Join Our Team</a></li>
-          <li><a className="link" href="#">Shop Under Armour</a></li>
-        </ul>
-      </div>
-      <div className="app-links-container">
-        <ul className="links-col">
-          <li className="link-header app-link-header">UA Fitness Community</li>
-          <div className="app-links">
-            <li className="app-icon record-icon" href="#"></li>
-            <li className="app-icon endo-icon" href="#"></li>
-            <li className="app-icon mfp-icon" href="#"></li>
-            <li className="app-icon mmf-icon" href="#"></li>
-          </div>
-        </ul>
-      </div>
-    </footer>
+  return (
+    <div>
+      <footer>
+        { phoneImage }
 
-    <div className="copyright">
-      <div className="copyright-text">
-          <span>
-            © {new Date().getFullYear()} MapMyJog® All rights reserved
-          </span>
-      </div>
+        <div className="links-container">
+          <ul className="links-col">
+            <li className="link-header">Social</li>
+            <li><a className="link" href="#">Like us on Facebook</a></li>
+            <li><a className="link" href="#">Follow us on Twitter</a></li>
+            <li><a className="link" href="#">MapMyRun Blog</a></li>
+          </ul>
+          <ul className="links-col">
+            <li className="link-header">Help</li>
+            <li><a className="link" href="#">Log In / Register</a></li>
+            <li><a className="link" href="#">Support</a></li>
+            <li><a className="link" href="#">Developer / API</a></li>
+          </ul>
+          <ul className="links-col">
+            <li className="link-header">About Us</li>
+            <li><a className="link" href="#">Advertise</a></li>
+            <li><a className="link" href="#">Join Our Team</a></li>
+            <li><a className="link" href="#">Shop Under Armour</a></li>
+          </ul>
+        </div>
+        <div className="app-links-container">
+          <ul className="links-col">
+            <li className="link-header app-link-header">UA Fitness Community</li>
+            <div className="app-links">
+              <li className="app-icon record-icon" href="#"></li>
+              <li className="app-icon endo-icon" href="#"></li>
+              <li className="app-icon mfp-icon" href="#"></li>
+              <li className="app-icon mmf-icon" href="#"></li>
+            </div>
+          </ul>
+        </div>
+      </footer>
 
-      <div className="copyright-link-container">
-        <a className="copyright-link" href="#">Privacy Policy</a>
-        <a className="copyright-link" href="#">Terms of Use</a>
-        <a className="copyright-link copyright-link-noborder" >AdChoices</a>
+      <div className="copyright">
+        <div className="copyright-text">
+            <span>
+              © {new Date().getFullYear()} MapMyJog® All rights reserved
+            </span>
+        </div>
+
+        <div className="copyright-link-container">
+          <a className="copyright-link" href="#">Privacy Policy</a>
+          <a className="copyright-link" href="#">Terms of Use</a>
+          <a className="copyright-link copyright-link-noborder" >AdChoices</a>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default Footer;
+export default withRouter(Footer);
